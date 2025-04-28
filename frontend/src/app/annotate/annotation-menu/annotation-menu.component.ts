f7bce94893acd9cd4252a1b1452135a0163abea2
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-04-28 11:14:12 +0200

    Extract input form from result tabs

--- frontend/src/app/annotate/annotation-menu/annotation-menu.component.ts
+++ frontend/src/app/annotate/annotation-menu/annotation-menu.component.ts
@@ -2,12 +2,10 @@ import { Component } from "@angular/core";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { NgbNavModule } from "@ng-bootstrap/ng-bootstrap";
 import {
-    faKeyboard,
     faSquarePollHorizontal,
     faTree,
     faPenNib,
 } from "@fortawesome/free-solid-svg-icons";
-import { AnnotationInputComponent } from "../annotation-input/annotation-input.component";
 import { AnnotationParseResultsComponent } from "../annotation-parse-results/annotation-parse-results.component";
 import { AnnotationTableauComponent } from "../annotation-tableau/annotation-tableau.component";
 import { AnnotationCommentsComponent } from "../annotation-comments/annotation-comments.component";
@@ -18,7 +16,6 @@ import { AnnotationCommentsComponent } from "../annotation-comments/annotation-c
     imports: [
         NgbNavModule,
         FontAwesomeModule,
-        AnnotationInputComponent,
         AnnotationParseResultsComponent,
         AnnotationTableauComponent,
         AnnotationCommentsComponent,
@@ -29,7 +26,6 @@ import { AnnotationCommentsComponent } from "../annotation-comments/annotation-c
 export class AnnotationMenuComponent {
     public active = 1;
 
-    public faKeyboard = faKeyboard;
     public faSquarePollHorizontal = faSquarePollHorizontal;
     public faTree = faTree;
     public faPenNib = faPenNib;

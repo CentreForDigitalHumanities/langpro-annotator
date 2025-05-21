4e3043c620f0e1f7334c0209458d74336326c2e3
Ben <b.bonfil@uu.nl>
2025-05-21 16:32:44 +0200

    wip drawing tableaus with svg

--- frontend/src/app/annotate/annotation-tableau/annotation-tableau.component.ts
+++ frontend/src/app/annotate/annotation-tableau/annotation-tableau.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
+import { TableauSVG } from "../tableau-svg/tableau-svg.component";
 
 @Component({
     selector: "la-annotation-tableau",
     standalone: true,
-    imports: [],
+    imports: [TableauSVG],
     templateUrl: "./annotation-tableau.component.html",
     styleUrl: "./annotation-tableau.component.scss",
 })
---
+++ frontend/src/app/annotate/tableau-svg/tableau-svg.component.ts
@@ -0,0 +1,210 @@
+import { Component, ElementRef, Input, Output, ViewChild, EventEmitter } from "@angular/core";
+
+interface Dimensions {
+    width?: number;
+    height?: number;
+}
+
+@Component({
+    selector: "[term]",
+    standalone: true,
+    imports: [],
+    templateUrl: "./tableau-svg-term.svg",
+})
+export class TableauTerm {
+    @Input()
+    public idx?: number;
+
+    @Input()
+    public label?: String;
+
+    @Input()
+    public term: String = '';
+
+    @Input()
+    public rule?: String;
+
+    @ViewChild('idxText')
+    idxText?: ElementRef<SVGTextElement>;
+
+    @ViewChild('termText')
+    termText!: ElementRef<SVGTextElement>;
+
+    @ViewChild('labelText')
+    labelText?: ElementRef<SVGTextElement>;
+
+    @Output()
+    public onSize = new EventEmitter<Dimensions>();
+
+    padding = 10;
+    termX: number = 0;
+    labelX: number = 0;
+    idxW: number = 0;
+    labelW: number = 0;
+    totalW: number = 0;
+
+    ngAfterViewChecked() {
+        this.idxW = this.idxText ? this.idxText.nativeElement.getComputedTextLength() + this.padding : 0;
+        this.labelW = this.labelText ? this.labelText.nativeElement.getComputedTextLength() + this.padding : 0;
+        this.termX = this.idxW;
+        this.labelX = this.termX + this.termText.nativeElement.getComputedTextLength() + this.padding;
+        this.totalW = this.labelW + this.idxW + this.termText.nativeElement.getComputedTextLength();
+
+        this.onSize.emit({width: this.totalW});
+    }
+
+}
+
+@Component({
+    selector: "[tableau-tree]",
+    standalone: true,
+    imports: [TableauTerm],
+    template: `
+<svg:g>
+  @for (node of tree.nodes; track tree.nodes; let i = $index) {
+<svg:g attr.transform="translate(0, {{ nodeY(i) }})">
+<svg:g term [idx]="node.idx" [term]="node.term" [label]="node.label" (onSize)="updateDimensions($event)" [rule]="node.rule"></svg:g>
+    @if (i < tree.nodes.length - 1) {
+<svg:path attr.d="M 0 {{ nodeHeight(node) - 15 }} l 0 15" stroke="blue" fill="none"/>
+    }
+    </svg:g>
+  }
+
+@if (tree.subtrees) {
+@for (subtree of tree.subtrees; track tree.subtrees; let i = $index) {
+<svg:path attr.d="
+M 0 {{ totalNodeHeight() - 15 }}
+q 0 {{levelHeight/2}} {{subtreePosition(i)/2 }} {{levelHeight/2}}
+q {{subtreePosition(i)/2}} 0 {{subtreePosition(i)/2}} {{levelHeight/2}}
+" stroke="blue" fill="none"/>
+}
+}
+
+<svg:g attr.transform="translate(0, {{ totalNodeHeight() + levelHeight }})">
+  @for (subtree of tree.subtrees; track tree.subtrees; let i = $index) {
+<svg:g tableau-tree [tree]="subtree" attr.transform="translate({{subtreePosition(i)}}, 0)"
+(onSize)="updateSubDimensions($event)"></svg:g>
+  }
+</svg:g>
+
+</svg:g>
+    `
+})
+export class TableauTree {
+    @Input()
+    tree: any;
+
+    levelHeight = 40;
+
+    width: number = 0;
+    subWidth: number = 0;
+    subHeight: number = 0;
+
+    @Output()
+    public onSize = new EventEmitter<Dimensions>();
+
+    updateDimensions(size: Dimensions) {
+        this.width = Math.max(this.width, size.width!);
+        this.emitSize();
+    }
+
+    updateSubDimensions(size: Dimensions) {
+        this.subWidth = Math.max(this.subWidth, size.width!);
+        this.subHeight = Math.max(this.subHeight, size.height!);
+        this.emitSize();
+    }
+
+    emitSize() {
+        this.onSize.emit({
+            width: Math.max(this.subWidth * (this.tree.subtrees?.length ?? 0), this.width),
+            height: this.subHeight + 70 * this.tree.nodes.length,
+        });
+    }
+
+    subtreePosition(idx: number) {
+        let widthWithPadding = 1.15 * this.subWidth;
+        return widthWithPadding * idx - (widthWithPadding / 2) * (this.tree.subtrees.length - 1);
+    }
+
+    nodeHeight(node: any) {
+        return node.rule ? 60 : 40;
+    }
+
+    totalNodeHeight() {
+        return this.tree.nodes.map(this.nodeHeight).reduce((sum: number, h: number) => sum + h, 0);
+    }
+
+    nodeY(idx: number) {
+        return this.tree.nodes.slice(0, idx).map(this.nodeHeight).reduce((sum: number, h: number) => sum + h, 0);
+    }
+}
+
+@Component({
+    selector: "la-tableau-svg",
+    standalone: true,
+    imports: [TableauTree],
+    templateUrl: "./tableau-svg.component.svg",
+    styleUrl: "./tableau-svg.component.scss",
+})
+export class TableauSVG {
+
+    treeDimensions: Dimensions = {width: 0, height: 0};
+
+    onTreeSize(size: Dimensions) {
+        this.treeDimensions = size;
+    }
+
+    tree: any = {
+        nodes: [
+            {idx: 1, term: 'every@man@(be@work)'},
+            {idx: 2, term: 'every@(who@(be@work)@person)@(λ1,a@(expensive@car)@(λ2,have@2@1))'},
+            {idx: 3, term: 'every@man@(λ3,a@car@(λ2,own@2@3))'},
+            {idx: 4, term: 'be@work', label: 'c1', rule: 'up_mon_fun_some[1,3]'},
+            {idx: 5, term: '(λ3,a@car@(λ2,own@2@3))', label: 'c1', rule: 'up_mon_fun_some[1,3]'},
+            {idx: 6, term: 'man', label: 'c1', rule: 'up_mon_fun_some[1,3]'},
+            {idx: 7, term: 'a@car@(λ2,own@2@c1)', rule: 'pull_arg[5]'},
+            {idx: 8, term: 'work', label: 'c1', rule: 'aux_verb[4]'},
+            {idx: 9, term: 'be@work', label: 'c1', rule: 'tr_every_c[1,6], [c1]'},
+            {idx: 8, term: 'work', label: 'c1', rule: 'aux_verb[9]'},
+        ],
+        subtrees: [
+            {
+                nodes: [
+                    {idx: 10, term: 'who@(be@work)@person', label: 'c1', rule: 'tr_every[2],[c1]'},
+                ],
+                subtrees: [
+                    {
+                        nodes: [
+                            {idx: 13, term: 'be@work', label: 'c1', rule: 'fl_conj_who[10]'},
+                            {idx: 16, term: 'work', label: 'c1', rule: 'aux_verb[13]'},
+                            {term: 'X', rule: 'cl_subcat[16,8]'}
+                        ],
+                    },
+                    {
+                        nodes: [
+                            {idx: 14, term: 'person', label: 'c1', rule: 'fl_conj_who[10]'},
+                            {term: 'X', rule: 'cl_subsumption[14,6]'}
+                        ]
+                    }
+                ]
+            },
+            {
+                nodes: [
+                    {idx: 11, term: 'who@(be@work)@person', label: 'c1', rule: 'tr_every[2],[c1]'},
+                    {idx: 12, term: '(λ1,a@(expensive@car)@(λ2,have@2@1))', label: 'c1', rule: 'tr_every[2],[c1]'},
+                    {idx: 15, term: 'a@(expensive@car)@(λ2,have@2@c1)', label: 'c1', rule: 'pull_arg[12]'},
+                    {idx: 17, term: 'be@work', label: 'c1', rule: 'tr_conj_who[11]'},
+                    {idx: 18, term: 'person', label: 'c1', rule: 'tr_conj_who[11]' },
+                    {idx: 8, term: 'work', label: 'c1', rule: 'aux_verb[17]'},
+                    {idx: 19, term: 'a@(expensive@car)', label: '(λ2,have@2@c1)', rule: 'same_args[15,7]'},
+                    {idx: 20, term: 'a@car', label: '(λ2,have@2@c1)', rule: 'same_args[15,7]'},
+                    {idx: 21, term: 'expensive@car', label: 'c2', rule: 'up_mon_fun[19,20]'},
+                    {idx: 22, term: 'car', label: 'c2', rule: 'up_mon_fun[19,20]'},
+                    {idx: 23, term: 'car', label: 'c2', rule:'int_mod_tr[21]'},
+                    {idx: 24, term: 'expensive', label: 'c2', rule:'int_mod_tr[21]'},
+                    {term: 'X', rule: 'cl_subsmption[23,22]'}
+                ]
+            }
+        ]
+    }
+}

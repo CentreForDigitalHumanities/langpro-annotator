10dfc239d35fb15eb81d9a5dc0a56aee8db157b8
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-08-12 21:22:19 +0200

    Remove unnecessary stringification

--- backend/problem/views/problem.py
+++ backend/problem/views/problem.py
@@ -43,14 +43,20 @@ class ProblemView(APIView):
         filters = get_filters(request.query_params)
 
         qs = Problem.objects.all()
+
         if filters is not None:
             qs = qs.filter(filters)
 
-        try:
-            problem = qs.get(id=problem_id)
-        except Problem.DoesNotExist:
-            # The selected problem may not be part of the selected filters.
-            # In that case, we simply take the first problem from the queryset.
+        problem = None
+        if problem_id is not None:
+            try:
+                problem = qs.get(id=problem_id)
+            except Problem.DoesNotExist:
+                # The selected problem may not be part of the selected filters.
+                # In that case, we simply take the first problem from the queryset.
+                pass
+
+        if problem is None:
             problem = qs.first()
 
         problem_index = problem.get_index() if problem else None
@@ -60,9 +66,9 @@ class ProblemView(APIView):
             id=problem.pk if problem else None,
             index=problem_index,
             problem=problem,
-            first=str(related_problem_ids.first),
-            previous=str(related_problem_ids.previous),
-            next=str(related_problem_ids.next),
-            last=str(related_problem_ids.last),
+            first=related_problem_ids.first,
+            previous=related_problem_ids.previous,
+            next=related_problem_ids.next,
+            last=related_problem_ids.last,
             total=related_problem_ids.total,
         ).json_response(status=200)

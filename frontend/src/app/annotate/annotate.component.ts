9d4ebf5749660c151e77223492d9128b1656697d
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-08-12 10:26:54 +0200

    Simply annotate component

--- frontend/src/app/annotate/annotate.component.ts
+++ frontend/src/app/annotate/annotate.component.ts
@@ -1,14 +1,10 @@
-import { Component, DestroyRef, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { AnnotationMenuComponent } from "./annotation-menu/annotation-menu.component";
 import { NavigatorComponent } from "./navigator/navigator.component";
 import { AnnotationInputComponent } from "./annotation-input/annotation-input.component";
-import { ActivatedRoute } from "@angular/router";
-import { AnnotateService } from "../services/annotate.service";
-import { map } from "rxjs";
-import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { SearchComponent } from "./search/search.component";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faTree } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
     selector: "la-annotate",
@@ -23,23 +19,6 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
     templateUrl: "./annotate.component.html",
     styleUrl: "./annotate.component.scss",
 })
-export class AnnotateComponent implements OnInit {
-    constructor(
-        private destroyRef: DestroyRef,
-        private route: ActivatedRoute,
-        private annotateService: AnnotateService
-    ) { }
-
-    public faCheck = faCheck;
-
-    ngOnInit(): void {
-        this.route.params
-            .pipe(
-                map((params) => params["problemId"]),
-                takeUntilDestroyed(this.destroyRef)
-            )
-            .subscribe((id) => {
-                this.annotateService.problemId.next(id);
-            });
-    }
+export class AnnotateComponent {
+    public faTree = faTree;
 }

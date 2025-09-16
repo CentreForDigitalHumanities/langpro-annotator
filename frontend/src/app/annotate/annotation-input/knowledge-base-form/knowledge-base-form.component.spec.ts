cf339b85a83e74f680235ba1647abfa49b5a1024
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-09-16 12:15:23 +0200

    Fix failing tests

--- frontend/src/app/annotate/annotation-input/knowledge-base-form/knowledge-base-form.component.spec.ts
+++ frontend/src/app/annotate/annotation-input/knowledge-base-form/knowledge-base-form.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { KnowledgeBaseFormComponent } from "./knowledge-base-form.component";
 import { FormArray, FormGroup } from "@angular/forms";
+import { provideHttpClient } from "@angular/common/http";
 
 describe("KnowledgeBaseFormComponent", () => {
     let component: KnowledgeBaseFormComponent;
@@ -10,6 +11,9 @@ describe("KnowledgeBaseFormComponent", () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [KnowledgeBaseFormComponent],
+            providers: [
+                provideHttpClient(),
+            ]
         }).compileComponents();
 
         fixture = TestBed.createComponent(KnowledgeBaseFormComponent);
--- frontend/src/app/services/app-mode.service.spec.ts
+++ frontend/src/app/services/app-mode.service.spec.ts
@@ -1,16 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AppModeService } from './app-mode.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AppModeService', () => {
-  let service: AppModeService;
+    let service: AppModeService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AppModeService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [provideHttpClient(), provideHttpClientTesting()],
+        });
+        service = TestBed.inject(AppModeService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });

e49e3d6d8a2e64c46890a4de6e6191eb87e6145a
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-11-04 14:39:46 +0100

    Add role field to User model

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,8 +1,43 @@
+from typing import Any
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
+from django.http import HttpRequest
 from . import models
+from django.utils.translation import gettext_lazy as _
 
 
 @admin.register(models.User)
 class UserAdmin(auth_admin.UserAdmin):
-    pass
+    def get_fieldsets(
+        self, request: HttpRequest, obj: models.User | None = None
+    ) -> list[tuple[str | None, dict[str, Any]]]:
+        user = request.user
+        if user.is_superuser:
+            admin_fieldset = super().get_fieldsets(request, obj)
+            if len(admin_fieldset) >= 3:
+                # Add the 'role' field to the admin fieldset for superusers.
+                admin_fieldset[2][1]["fields"] = (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "role",
+                    "groups",
+                    "user_permissions",
+                )
+            return admin_fieldset
+
+        # Non-superusers should not be able to edit password, is_superuser and groups through Django Admin.
+        return [
+            (None, {"fields": ("username",)}),
+            (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+            (
+                _("Permissions"),
+                {
+                    "fields": (
+                        "is_active",
+                        "is_staff",
+                        "role",
+                    ),
+                },
+            )
+        ]
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,4 +1,5 @@
 import django.contrib.auth.models as django_auth_models
+from django.db import models
 
 
 class User(django_auth_models.AbstractUser):
@@ -12,3 +13,21 @@ class User(django_auth_models.AbstractUser):
 
     class Meta:
         db_table = "auth_user"
+
+    class Role(models.TextChoices):
+        ANNOTATOR = "annotator", "Annotator"
+        MASTER_ANNOTATOR = "master_annotator", "Master Annotator"
+        VISITOR = "visitor", "Visitor"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.VISITOR,
+        help_text="Visitors can browse problems and parses. Annotators can annotate non-locked problems. Master Annotators can manage users, change problem lock status, and review annotations.",
+    )
+    @property
+    def can_edit_or_add_problem(self) -> bool:
+        """
+        Determines whether the user can edit or add problems.
+        """
+        return self.is_superuser or self.role in [self.Role.MASTER_ANNOTATOR]

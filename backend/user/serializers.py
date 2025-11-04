e940059411158cecfcc6548ad32b55f0acddc3ae
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-11-04 16:58:17 +0100

    Fix backend tests

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 
 
 class CustomUserDetailsSerializer(UserDetailsSerializer):
-    firstName = serializers.CharField(read_only=True, source='first_name')
-    lastName = serializers.CharField(read_only=True, source='last_name')
+    firstName = serializers.CharField(source='first_name')
+    lastName = serializers.CharField(source='last_name')
     isStaff = serializers.BooleanField(read_only=True, source='is_staff')
     canEditOrAddProblem = serializers.BooleanField(read_only=True, source='can_edit_or_add_problem')
 

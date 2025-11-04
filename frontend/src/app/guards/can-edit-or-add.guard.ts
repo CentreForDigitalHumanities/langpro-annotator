a5c44c50e3c02aad6e6c9f9bbaaa2b58e2e4c44a
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-11-04 15:35:21 +0100

    Wait in CanEditOrAddGuard

--- frontend/src/app/guards/can-edit-or-add.guard.ts
+++ frontend/src/app/guards/can-edit-or-add.guard.ts
@@ -1,12 +1,15 @@
 import { AuthService } from "@/services/auth.service";
 import { inject } from "@angular/core";
 import { CanActivateFn } from "@angular/router";
-import { map } from "rxjs";
+import { filter, map, take } from "rxjs";
 
 export const CanEditOrAddGuard: CanActivateFn = () => {
     const authService = inject(AuthService);
 
     return authService.currentUser$.pipe(
+        // Wait until we actually have a user (User | null).
+        filter(user => user !== undefined),
+        take(1),
         map(user => user?.canEditOrAddProblem ?? false),
     );
 };

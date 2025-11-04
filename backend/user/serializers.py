d62706045188c686d1571e21082455f8534bdb41
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-11-04 14:40:17 +0100

    Add role to serializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -5,7 +5,11 @@ from rest_framework import serializers
 class CustomUserDetailsSerializer(UserDetailsSerializer):
 
     class Meta(UserDetailsSerializer.Meta):
-        is_staff = serializers.BooleanField(read_only=True)
+        firstName = serializers.CharField(read_only=True, source='first_name')
+        lastName = serializers.CharField(read_only=True, source='last_name')
+        isStaff = serializers.BooleanField(read_only=True, source='is_staff')
+        canEditOrAddProblem = serializers.BooleanField(read_only=True, source='can_edit_or_add_problem')
+
         fields = (
             "id",
             "username",
@@ -13,5 +17,6 @@ class CustomUserDetailsSerializer(UserDetailsSerializer):
             "first_name",
             "last_name",
             "is_staff",
+            "role",
         )
         read_only_fields = ["is_staff", "id", "email"]

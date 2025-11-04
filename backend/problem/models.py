2663a2950573a095310545b4247b11646642c300
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-11-04 10:53:46 +0100

    Remove unused import

--- backend/problem/models.py
+++ backend/problem/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
 from django.db.models import QuerySet
 
 from problem.services import FracasData, SNLIData, SickData

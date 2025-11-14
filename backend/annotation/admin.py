72b2d5459145e96c89f98df38927f35d10a9162b
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-11-14 09:33:27 +0100

    Update fixtures; create Admin interface

--- backend/annotation/admin.py
+++ backend/annotation/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from annotation.models import Label
+
+
 # Register your models here.
+@admin.register(Label)
+class LabelAdmin(admin.ModelAdmin):
+    list_display = ("text", "description")
+    search_fields = ("text",)

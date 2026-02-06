3b52f4996a9d1f6d8573188b975663e1416df7c3
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2026-02-06 16:39:53 +0100

    Fix frontend test

--- frontend/src/app/annotate/annotation-parse-results/parse-tree-table/parse-tree-table.component.spec.ts
+++ frontend/src/app/annotate/annotation-parse-results/parse-tree-table/parse-tree-table.component.spec.ts
@@ -1,6 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ParseTreeTableComponent } from './parse-tree-table.component';
+import { ParseTree, ParseTreeType } from '../types';
+
+const mockTree: ParseTree = {
+    type: ParseTreeType.CCG_DERIVATION,
+    root: {
+        type: "leaf",
+        lem: "dog",
+        tok: "Dog",
+        pos: "NN",
+        ner: "O",
+        cat: "N"
+    }
+}
 
 describe('ParseTreeTableComponent', () => {
     let component: ParseTreeTableComponent;
@@ -14,7 +27,13 @@ describe('ParseTreeTableComponent', () => {
 
         fixture = TestBed.createComponent(ParseTreeTableComponent);
         component = fixture.componentInstance;
+        const componentRef = fixture.componentRef;
+        componentRef.setInput(
+            "tree",
+            mockTree
+        );
         fixture.detectChanges();
+
     });
 
     it('should create', () => {

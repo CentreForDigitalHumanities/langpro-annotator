5383b4572c70d1d227b01a4fd843d53ced57e428
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2026-03-13 15:34:35 +0100

    Use parse results reactively in AnnotationParseResults template

--- frontend/src/app/annotate/annotation-parse-results/annotation-parse-results.component.ts
+++ frontend/src/app/annotate/annotation-parse-results/annotation-parse-results.component.ts
@@ -1,31 +1,26 @@
-import { Component, DestroyRef, inject, OnInit } from "@angular/core";
+import { Component, DestroyRef, inject } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
-import { mockResult } from "./mockParseResult";
 import { ParseService } from "@/services/parse.service";
 import { ParseTreeTableComponent } from "./parse-tree-table/parse-tree-table.component";
 import { NgbAccordionModule } from "@ng-bootstrap/ng-bootstrap";
+import { map } from "rxjs";
+import { CommonModule } from "@angular/common";
 
 
 @Component({
     selector: "la-annotation-parse-results",
     standalone: true,
-    imports: [ParseTreeTableComponent, NgbAccordionModule],
+    imports: [ParseTreeTableComponent, NgbAccordionModule, CommonModule],
     templateUrl: "./annotation-parse-results.component.html",
     styleUrl: "./annotation-parse-results.component.scss",
 })
-export class AnnotationParseResultsComponent implements OnInit {
+export class AnnotationParseResultsComponent {
     private destroyRef = inject(DestroyRef);
     private parseService = inject(ParseService);
 
-    public parseResults = mockResult;
-
-    ngOnInit(): void {
-        // Subscription needed to ensure a request is actually made.
-        this.parseService.parse$
-            .pipe(takeUntilDestroyed(this.destroyRef))
-            .subscribe((response) => {
-                console.log("Parse response:", response);
-                this.parseResults = response.data.ccg_trees;
-            });
-    }
+    public parseResults$ = this.parseService.parse$
+        .pipe(
+            map(response => response?.data || null),
+            takeUntilDestroyed(this.destroyRef)
+        );
 }

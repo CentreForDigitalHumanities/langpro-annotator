79164573286ede35b32807044aaeb2240e047046
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2026-03-17 09:30:44 +0100

    Clear parse results upon navigating to a new problem

--- frontend/src/app/services/parse.service.ts
+++ frontend/src/app/services/parse.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Subject, switchMap, catchError, of } from 'rxjs';
+import { Subject, switchMap, catchError, of, merge, map } from 'rxjs';
 import { ParseInput } from '@/annotate/annotation-input/annotation-input.component';
 import { ParseResponseData } from '@/annotate/annotation-parse-results/types';
+import { ProblemService } from './problem.service';
 
 
 export type ParseResponse = {
@@ -15,10 +16,14 @@ export type ParseResponse = {
 })
 export class ParseService {
     private http = inject(HttpClient);
+    private problemService = inject(ProblemService);
 
     public submit$ = new Subject<ParseInput>();
 
-    public parse$ = this.submit$.pipe(
+    // Clear parse results when a new problem is loaded.
+    private clearOnNewProblem$ = this.problemService.problemResponse$.pipe(map(() => null));
+
+    private parseResults$ = this.submit$.pipe(
         switchMap((form) =>
             this.http.post<ParseResponse>("/api/problem/parse", form).pipe(
                 catchError((error) => {
@@ -27,5 +32,10 @@ export class ParseService {
                 }),
             )
         )
+    );
+
+    public parse$ = merge(
+        this.parseResults$,
+        this.clearOnNewProblem$
     );
 }

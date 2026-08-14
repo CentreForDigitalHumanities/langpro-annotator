47c2168577d275e0512f14ef00d51824fe64a87d
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2026-08-14 15:50:58 +0200

    Fix frontend tests

--- frontend/src/app/annotate/annotation-input/langpro-prediction/langpro-prediction.component.spec.ts
+++ frontend/src/app/annotate/annotation-input/langpro-prediction/langpro-prediction.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LangProPredictionComponent } from './langpro-prediction.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('LangProPredictionComponent', () => {
     let component: LangProPredictionComponent;
@@ -8,7 +9,8 @@ describe('LangProPredictionComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [LangProPredictionComponent]
+            imports: [LangProPredictionComponent],
+            providers: [provideHttpClient()],
         })
             .compileComponents();
 
